1d7fd14b2c86c645a60eb2fdd0aa45778d52b672
Alex Rojas <alex.olmedo.rojas@gmail.com>
2023-03-19 20:41:30 -0600

    Added imports and state to SignIn page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
 function SignIn() {
-    return (
-      <h1>Sign In</h1>
-    )
-  }
-  
-  export default SignIn
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const navigate = useNavigate();
+
+  return <h1>Sign In</h1>;
+}
+
+export default SignIn;
